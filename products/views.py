4aa2b427fd546da2d3d5bee7c46b1718d1ea5955
ldiur <lioneldiur@icloud.com>
2023-06-28 23:19:46 +0200

    initial commit

---
+++ products/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from .models import Product
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html',
+                  {'products': products})
+
+
+def new(request):
+    return HttpResponse('New product')
+
